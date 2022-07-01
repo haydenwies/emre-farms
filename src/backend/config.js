08f8abc95b463beee51b1a8d803af82bac1d526b
Hayden Wies <haydenwies@gmail.com>
2022-07-01 13:40:09 -0400

    Moved api key to .env file.

--- src/backend/config.js
+++ src/backend/config.js
@@ -2,9 +2,16 @@ import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
-import { firebaseConfig } from '../secrets/secrets';
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+});
 
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
